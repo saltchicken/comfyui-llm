0a3aafc9bf28f4eaa8993386af1c2b269adec64e
saltchicken <john.eicher89@gmail.com>
2025-03-15 13:55:52 -0700

    In case system_message is not enterred, revert to None

--- nodes.py
+++ nodes.py
@@ -11,7 +11,7 @@ class OllamaQuery:
             "required": {
                 # "model": (get_ollama_models(),),
                 "model": ("STRING", {"default": "gemma3:1b"}),
-                "system_message": ("STRING", {"default": "You are a helpful assistant."}),
+                "system_message": ("STRING", {"default": ""}),
                 "prompt": ("STRING", {"default": "Hello, world!"}),
                 "host"  : ("STRING", {"default": "localhost"}),
                 "port" : ("STRING", {"default": 11434}),
@@ -23,10 +23,13 @@ class OllamaQuery:
     CATEGORY = "LLM"
     TITLE = "Ollama Query"
 
-    def process(self, model: str, system_message: str, prompt: str, host: str, port: str):
+    def process(self, model: str, system_message: any, prompt: str, host: str, port: str):
         """
         Query Ollama with a given prompt.
         """
+        if system_message == "":
+            system_message = None
+
         response, debug_text = ollama_query(model=model, prompt=prompt, system_message=system_message, host=host, port=port)
 
         return (response, debug_text)
